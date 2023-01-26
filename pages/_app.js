dffb93ca1b1fbd5af063029bd93bb7a06cfe8802
Johnathan Reese <JohnathanReese8@gmail.com>
2023-01-25 20:46:13 -0500

    Part 1 of tutorial done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+const theme = extendTheme({
+    styles: {
+        global: {
+            body: {
+                backgroundColor: '8fc2e5',
+            }
+        }
+    }
+})
+
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider>
